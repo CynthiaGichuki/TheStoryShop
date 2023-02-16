3b05df9218c1c80f351813b3971042830ad2ae04
samuel nderitu waweru <samuelnderitu495@gmail.com>
2023-02-16 19:51:23 +0300

    Added authentication module and Cart backend

---
+++ backend/database/tables/cart.sql
@@ -0,0 +1,17 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cart](
+[cart_ID] [int] IDENTITY(1,1) NOT NULL,
+[user_ID] varchar NOT NULL,
+[product_ID] varchar NOT NULL,
+[quantity] [int] NOT NULL,
+[total_price] [decimal](18, 2) NOT NULL,
+[created_at] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED
+(
+[cart_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+G
